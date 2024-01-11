5dd99cc5bac0a84624e27133587bea4e56dff90e
Lawrence Amoafo <rekenburg777@gmail.com>
2024-01-11 01:32:56 +0000

    Create new routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {:registrations => "registrations"}
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  root 'users#splash'
+
+  resources :searches, only: [:index, :create]
 end
